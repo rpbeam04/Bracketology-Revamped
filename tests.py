8889f2272fd674a4986bd08beee5033bdc4b186f
rpbeam04 <rpbeam04@gmail.com>
2024-01-23 09:53:26 -0600

    Testing future game scraping

--- tests.py
+++ tests.py
@@ -19,12 +19,14 @@ Team.create_teams_from_stats(gender = "women", write_to_json=True)
 print(len(Team.team_list))
 Team.clean_duplicates()
 
-games = fetch.fetch_games(start_date=dt(2023,12,1),end_date=dt(2024,1,10),sleep=10)
+#games = fetch.fetch_games(start_date=dt(2023,12,1),end_date=dt(2024,1,10),sleep=10)
+games = fetch.fetch_games_on_date(dt(2024,2,1))
 Game.write_games_to_json()
 
 print(len(Game.game_list))
 
 team: Team
 for team in Team.team_list:
-    if len(team.Games) < 1 or len(team.Games) > 30:
-        print(team.Name, team.Gender, len(team.Games))
+    # if len(team.Games) < 1 or len(team.Games) > 30:
+    #     print(team.Name, team.Gender, len(team.Games))
+    pass
